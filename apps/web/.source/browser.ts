e0ad8bba74650a3441f5bd56a89c790a42c8661d
Charlie <CharlieLSparrow@gmail.com>
2026-03-18 08:06:08 -0600

    add first three episodes and opening book sections

--- apps/web/.source/browser.ts
+++ apps/web/.source/browser.ts
@@ -7,6 +7,6 @@ const create = browser<typeof Config, import("fumadocs-mdx/runtime/types").Inter
   }
 }>();
 const browserCollections = {
-  docs: create.doc("docs", {"index.mdx": () => import("../content/docs/index.mdx?collection=docs"), "preface.mdx": () => import("../content/docs/preface.mdx?collection=docs"), }),
+  docs: create.doc("docs", {"chapter-zero-in-the-beginning.mdx": () => import("../content/docs/chapter-zero-in-the-beginning.mdx?collection=docs"), "episode-001.mdx": () => import("../content/docs/episode-001.mdx?collection=docs"), "episode-002.mdx": () => import("../content/docs/episode-002.mdx?collection=docs"), "episode-003.mdx": () => import("../content/docs/episode-003.mdx?collection=docs"), "index.mdx": () => import("../content/docs/index.mdx?collection=docs"), "introduction.mdx": () => import("../content/docs/introduction.mdx?collection=docs"), "preface.mdx": () => import("../content/docs/preface.mdx?collection=docs"), }),
 };
 export default browserCollections;
--- apps/web/.source/server.ts
+++ apps/web/.source/server.ts
@@ -1,6 +1,11 @@
 // @ts-nocheck
-import * as __fd_glob_2 from "../content/docs/preface.mdx?collection=docs"
-import * as __fd_glob_1 from "../content/docs/index.mdx?collection=docs"
+import * as __fd_glob_7 from "../content/docs/preface.mdx?collection=docs"
+import * as __fd_glob_6 from "../content/docs/introduction.mdx?collection=docs"
+import * as __fd_glob_5 from "../content/docs/index.mdx?collection=docs"
+import * as __fd_glob_4 from "../content/docs/episode-003.mdx?collection=docs"
+import * as __fd_glob_3 from "../content/docs/episode-002.mdx?collection=docs"
+import * as __fd_glob_2 from "../content/docs/episode-001.mdx?collection=docs"
+import * as __fd_glob_1 from "../content/docs/chapter-zero-in-the-beginning.mdx?collection=docs"
 import { default as __fd_glob_0 } from "../content/docs/meta.json?collection=docs"
 import { server } from 'fumadocs-mdx/runtime/server';
 import type * as Config from '../source.config';
@@ -10,4 +15,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, }, {"index.mdx": __fd_glob_1, "preface.mdx": __fd_glob_2, });
+export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, }, {"chapter-zero-in-the-beginning.mdx": __fd_glob_1, "episode-001.mdx": __fd_glob_2, "episode-002.mdx": __fd_glob_3, "episode-003.mdx": __fd_glob_4, "index.mdx": __fd_glob_5, "introduction.mdx": __fd_glob_6, "preface.mdx": __fd_glob_7, });
