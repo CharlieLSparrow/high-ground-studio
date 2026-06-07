cfde4d9e9d8e00733dd3bc385050ba60a9d49839
Charlie <CharlieLSparrow@gmail.com>
2026-06-07 15:22:34 -0600

    Migrate Quipsly middleware to proxy

--- apps/quipsly/src/proxy.ts
+++ apps/quipsly/src/proxy.ts
@@ -36,7 +36,7 @@ function isPublicAssetPath(pathname: string) {
   )
 }
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const url = request.nextUrl
   const hostname = normalizeHost(request.headers.get('host'))
 
